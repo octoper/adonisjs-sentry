f38670f519a445384c916f3a0ad5b75408c6786e
Vaggelis Yfantis <me@octoper.me>
2024-03-24 19:21:29 +0200

    Update provider

--- providers/sentry_provider.ts
+++ providers/sentry_provider.ts
@@ -1,20 +1,26 @@
 import type { ApplicationService } from '@adonisjs/core/types'
 import * as Sentry from '@sentry/node'
 
+declare module '@adonisjs/core/types' {
+  export interface ContainerBindings {
+    'sentry': typeof Sentry
+  }
+}
+
 /**
- * Inertia provider
+ * Sentry provider
  */
 export default class SentryProvider {
   constructor(protected app: ApplicationService) {}
 
   async register() {
-    this.app.container.singleton(Sentry, () => {
+    this.app.container.singleton('sentry', () => {
       return { ...Sentry }
     })
   }
 
   async boot() {
-    const config = this.app.config.get('sentry')
+    const config = this.app.config.get<Sentry.NodeOptions|undefined>('sentry')
     Sentry.init(config)
   }
 }
